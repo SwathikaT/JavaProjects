dc95bbf50ef2df9a53d7baa9778b00e01bd0f48d
Swathika T <swathika003@gmail.com>
2018-07-25 17:23:32 +0545

    Encapsulation commit

---
+++ Encapsulation/src/com/deloitte/payroll/Employee.java
@@ -0,0 +1,37 @@
+package com.deloitte.payroll;
+
+public class Employee {
+	private int empId;
+	private String fName;
+	private String lName;
+	private int deptId;
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	
+	
+}
