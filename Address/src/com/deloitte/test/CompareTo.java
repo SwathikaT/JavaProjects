9420f467065fba3ef2108301f1187c5b607aea81
Swathika T <swathika003@gmail.com>
2018-07-26 17:49:08 +0530

    commit

---
+++ Address/src/com/deloitte/test/CompareTo.java
@@ -0,0 +1,11 @@
+package com.deloitte.test;
+
+public class CompareTo {
+	 static String str1 = "abc";
+	 static String str2="def";
+	
+	public static void main(String[] args) {
+		System.out.println( str1.compareTo(str2));
+	}
+
+}
