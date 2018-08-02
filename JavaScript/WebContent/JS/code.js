df05a31fa457d99d5a94800b43611c671743fbf1
Swathika T <swathika003@gmail.com>
2018-08-02 19:25:16 +0530

    JS commit

---
+++ JavaScript/WebContent/JS/code.js
@@ -0,0 +1,39 @@
+function display(){
+	var hd = document.getElementById('hellodiv');
+	hd.innerHTML='Clicked....';
+}
+
+
+
+
+//window.document.write('Hello, World');
+//window.alert('Alert!')
+function greetings(){
+//sum(2,3);
+//sum('2','3');
+	//alert(document.loginform.user.value);
+	var sum =Myfunction(10,20);
+	
+}
+
+/*function sum(num1,num2){
+	//alert(num1+num2);
+	var result=num1+num2;
+	alert(result);
+}*/
+
+var Myfunction = function(var1,var2){
+	//return(var1+var2);
+	alert(var1+var2);
+	}
+
+function test(){
+	var var1=1;
+}
+test();
+function test2() {
+	alert(var1);
+}
+test2();
+
+
